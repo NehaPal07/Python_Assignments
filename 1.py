d254c3b7b8ddfd5932c1f990fb5a9a8aeac06123
NehaPal07 <31745036+NehaPal07@users.noreply.github.com>
2020-07-13 09:48:39 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,24 @@
+﻿Return the number of items in a list.
+
+mylist = ["apple", "banana", "cherry"]
+x = len(mylist)
+
+print(x)
+
+3
+
+round() is a built-in function in Python. It returns x rounded to n digits from the decimal point
+print ("round(70.23456) : ", round(70.23456))
+print ("round(56.659,1) : ", round(56.659,1))
+print ("round(-100.000056, 3) : ", round(-100.000056, 3))
+
+round(70.23456) :  70
+round(56.659,1) :  56.7
+round(-100.000056, 3) :  -100.0
+
+The abs() method returns the absolute value of x i.e. the positive distance between x and zero.
+print ("abs(-45) : ", abs(-45))
+print ("abs(100.12) : ", abs(100.12))
+
+abs(-45) :  45
+abs(100.12) :  100.12
---
+++ 3.py
@@ -0,0 +1,10 @@
+﻿def toString(arr):
+    s = ''
+    for i in range(len(arr)):
+        if i > 0:
+            if i == len(arr) - 1:
+                s = s + ' and '
+            else:
+                s = s + ', '
+        s = s + arr[i];
+    return s
